8d5bd67d3e954837ad323baa629315c80c9ef604
Nitisha Bhattarai <nitishaabhattarai@gmail.com>
2022-07-31 23:14:24 +0545

    initial structure

---
+++ src/Components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default FooterComponent
---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./index.scss"
+
+const HeaderComponent = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <li>होमपेज</li>
+          <li>समाचार</li>
+          <li>विजनेस</li>
+          <li>जीवनशैली</li>
+          <li>विचार</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default HeaderComponent;
