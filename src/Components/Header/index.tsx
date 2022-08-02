cab31bfc7c4c12dc8c2940835b9c46ce1dd306e0
Nitisha Bhattarai <nitishaabhattarai@gmail.com>
2022-08-02 18:34:43 +0545

    header fixes

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,20 +1,34 @@
-import React, { useState } from "react";
-import "./header.scss"
+import React, { useEffect, useState } from "react";
+import NepaliDate from "nepali-date-converter";
+import HamBurger from "../../assets/images/hamburger.svg";
+import "./header.scss";
+
+  var NepaliWords = require('nep-eng');
 
 const HeaderComponent = () => {
- const [currentDate, setCurrentDate]= useState(Date().toLocaleString()) 
+  const [currentDate, setCurrentDate] = useState(
+    new NepaliDate().toLocaleString()
+  );
+  const s = NepaliWords.translate('currentDate')
+useEffect(()=>{
+ 
+
+},[])
   return (
     <div className="header">
-      <div className="top-section container"> <span>{currentDate}</span> <img src="/logo.jpg" alt="logo"/></div>
-
+      <div className="top-section container">
+        {s}
+        <span>{currentDate}</span> <img src="/logo.jpg" alt="logo" />
+      </div>
       <nav>
         <ul>
-          <li>होमपेज</li>
+          <li className="active">होमपेज</li>
           <li>समाचार</li>
           <li>विजनेस</li>
           <li>जीवनशैली</li>
           <li>विचार</li>
         </ul>
+        <img src={HamBurger} alt="ham-menu" />
       </nav>
     </div>
   );
